5ef2152e300180f13436000770878753f6b59d2d
15-2505-040-1 <v040ff@yamaguchi-u.ac.jp>
2017-09-22 16:41:25 +0900

    末吉が編集

--- App/Sheep/app/src/main/java/com/example/v040ff/sheep/MainActivity.java
+++ App/Sheep/app/src/main/java/com/example/v040ff/sheep/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.v040ff.sheep;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onStartButtonTapped(View view){
+        Intent intent=new Intent(this, courseSentaku.class);
+        startActivity(intent);
     }
 }
