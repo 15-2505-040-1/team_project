e136a870a9159b0882af1e7dffb260367e1f1b80
enPiT-5 <v018ff@yamaguchi-u.ac.jp>
2017-09-23 10:09:33 +0900

    岡部

--- App/Sheep/app/src/main/java/com/example/v040ff/sheep/MainActivity.java
+++ App/Sheep/app/src/main/java/com/example/v040ff/sheep/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.v040ff.sheep;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,9 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void onStartButtuonTapped(View view){
-        Intent intent = new Intent(this, courseSentaku.class);
-        startActivity(intent);
     }
 }
