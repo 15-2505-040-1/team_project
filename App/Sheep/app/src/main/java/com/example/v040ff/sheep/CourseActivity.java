650af0290e6b287d2e7e7f037300ea22d2c5e2a8
15-2505-040-1 <v040ff@yamaguchi-u.ac.jp>
2017-09-23 09:51:58 +0900

    画面遷移

--- App/Sheep/app/src/main/java/com/example/v040ff/sheep/CourseActivity.java
+++ App/Sheep/app/src/main/java/com/example/v040ff/sheep/CourseActivity.java
@@ -1,7 +1,9 @@
 package com.example.v040ff.sheep;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class CourseActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class CourseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course);
+    }
+    public void onStartButtonTapped(View view){
+        Intent intent=new Intent(this, LevelActivity.class);
+        startActivity(intent);
     }
 }
--- App/Sheep/app/src/main/java/com/example/v040ff/sheep/LevelActivity.java
+++ App/Sheep/app/src/main/java/com/example/v040ff/sheep/LevelActivity.java
@@ -3,11 +3,11 @@ package com.example.v040ff.sheep;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class nanidoSentaku extends AppCompatActivity {
+public class LevelActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nanido_sentaku);
+        setContentView(R.layout.activity_level);
     }
 }
