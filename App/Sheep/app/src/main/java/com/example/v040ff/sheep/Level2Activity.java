77be5d6a5dd2fad714680deb5d91e2b813937bb5
15-2505-040-1 <v040ff@yamaguchi-u.ac.jp>
2017-09-23 13:20:24 +0900

    画面遷移

--- App/Sheep/app/src/main/java/com/example/v040ff/sheep/Level2Activity.java
+++ App/Sheep/app/src/main/java/com/example/v040ff/sheep/Level2Activity.java
@@ -5,23 +5,23 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class LevelActivity extends AppCompatActivity {
-    int x=0;
+public class Level2Activity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_level);
+        setContentView(R.layout.activity_level2);
     }
     public void onLevel1ButtonTapped(View view){
-        Intent intent=new Intent(this, ProbActivity.class);
+        Intent intent=new Intent(this, Prob4Activity.class);
         startActivity(intent);
     }
     public void onLevel2ButtonTapped(View view){
-        Intent intent=new Intent(this, Prob2Activity.class);
+        Intent intent=new Intent(this, Prob5Activity.class);
         startActivity(intent);
     }
     public void onLevel3ButtonTapped(View view){
-        Intent intent=new Intent(this, Prob3Activity.class);
+        Intent intent=new Intent(this, Prob6Activity.class);
         startActivity(intent);
     }
 }
